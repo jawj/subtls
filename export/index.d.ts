66c4fbb01e295c9c115c6876b293696fcc75d8e0
George MacKerron <george@mackerron.co.uk>
2022-12-14 21:44:11 +0000

    Exporting ReadQueue

--- export/index.d.ts
+++ export/index.d.ts
@@ -4,6 +4,12 @@ export declare class TrustedCert {
   description(): string;
 }
 
+export declare class ReadQueue {
+  constructor(private ws: WebSocket);
+  async read(bytes: number): Promise<Uint8Array | undefined>;
+  bytesInQueue(): number;
+}
+
 export declare function startTls(
   host: string,
   rootCerts: TrustedCert[],
@@ -12,4 +18,4 @@ export declare function startTls(
   useSNI?: boolean,
   writePreData?: Uint8Array,
   expectPreData?: Uint8Array,
-): Promise<readonly [() => Promise<Uint8Array>, (data: Uint8Array) => Promise<void>]>;
+): Promise<readonly [() => Promise<Uint8Array | undefined>, (data: Uint8Array) => Promise<void>]>;
--- src/export.ts
+++ src/export.ts
@@ -2,4 +2,3 @@
 export { startTls } from './tls/startTls';
 export { TrustedCert } from './tls/cert';
 export { ReadQueue } from './util/readqueue';
-export { Bytes } from './util/bytes';
