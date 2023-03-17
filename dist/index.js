050140a493f7193a4c76694cccc2ebf7b2919656
George MacKerron <george@mackerron.co.uk>
2023-03-17 16:34:58 +0000

    Fixed Postgres message

--- dist/index.js
+++ dist/index.js
@@ -2166,14 +2166,6 @@ var pg = urlStr && urlStr.startsWith("postgres");
 var goBtn = document.getElementById("go");
 if (pg) {
   goBtn.value = "Ask Postgres the time over TLS";
-  document.getElementById("extra").innerHTML = `
-    <b>Postgres version</b> &nbsp; 
-    To minimise latency, we pipeline the outgoing Postgres traffic into only three transmissions. 
-    The first combines a Postgres SSL request and (without awaiting a reply) a TLS ClientHello. 
-    The second combines a Postgres startup message, a password message and a SELECT query. 
-    The third simply closes the connection. 
-    (You can follow this I/O by opening Chrome\u2019s developer tools, picking the 
-    Network tab, and selecting the WebSocket connection: <code>v1?address=...</code>).`;
 }
 goBtn.addEventListener("click", () => {
   if (pg)
--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ const goBtn = document.getElementById('go')! as HTMLButtonElement;
 
 if (pg) {
   goBtn.value = 'Ask Postgres the time over TLS';
-  document.getElementById('extra')!.innerHTML = `
-    <b>Postgres version</b> &nbsp; 
-    To minimise latency, we pipeline the outgoing Postgres traffic into only three transmissions. 
-    The first combines a Postgres SSL request and (without awaiting a reply) a TLS ClientHello. 
-    The second combines a Postgres startup message, a password message and a SELECT query. 
-    The third simply closes the connection. 
-    (You can follow this I/O by opening Chrome’s developer tools, picking the 
-    Network tab, and selecting the WebSocket connection: <code>v1?address=...</code>).`
+  // document.getElementById('extra')!.innerHTML = `
+  //   <b>Postgres version</b> &nbsp; 
+  //   To minimise latency, we pipeline the outgoing Postgres traffic into only three transmissions. 
+  //   The first combines a Postgres SSL request and (without awaiting a reply) a TLS ClientHello. 
+  //   The second combines a Postgres startup message, a password message and a SELECT query. 
+  //   The third simply closes the connection. 
+  //   (You can follow this I/O by opening Chrome’s developer tools, picking the 
+  //   Network tab, and selecting the WebSocket connection: <code>v1?address=...</code>).`
 }
 
 goBtn.addEventListener('click', () => {
