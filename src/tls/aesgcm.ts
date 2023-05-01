2cf9945e22ed1f8725611e03081ad92a1bec477e
George MacKerron <george@mackerron.co.uk>
2023-05-01 17:08:00 +0100

    Fixed encrypt/decrypt Promise resolution sequencing

--- src/tls/aesgcm.ts
+++ src/tls/aesgcm.ts
@@ -1,11 +1,10 @@
 import cs from '../util/cryptoProxy';
-// import { log } from '../presentation/log';
 
 const maxRecords = (2 ** 31) - 1;  // because JS bit-twiddling is done on signed Int32
 
 export class Crypter {
   recordsProcessed = 0;
-  prevPromise: Promise<any> = Promise.resolve();
+  priorPromise = Promise.resolve(new Uint8Array());
 
   constructor(
     private mode: 'encrypt' | 'decrypt',
@@ -13,8 +12,13 @@ export class Crypter {
     private initialIv: Uint8Array
   ) { }
 
-  // data is plainText for encrypt, concat(ciphertext, authTag) for decrypt
+  // this wrapper ensures returned Promises always resolve in sequence (which is otherwise not guaranteed in Node)
   async process(data: Uint8Array, authTagLength: number, additionalData: Uint8Array) {
+    return this.priorPromise = this.priorPromise.then(() => this._process(data, authTagLength, additionalData));
+  }
+
+  // data is plainText for encrypt, concat(ciphertext, authTag) for decrypt
+  async _process(data: Uint8Array, authTagLength: number, additionalData: Uint8Array) {
     const record = this.recordsProcessed;
     if (record === maxRecords) throw new Error('Cannot encrypt/decrypt any more records');
     this.recordsProcessed += 1;
@@ -26,25 +30,10 @@ export class Crypter {
     iv[ivLength - 3] ^= record >>> 16 & 0xff;
     iv[ivLength - 4] ^= record >>> 24 & 0xff;
 
-    // chatty && log(`records ${this.mode}ed:`, this.recordsCrypted);
-
     const tagLength = authTagLength << 3;  // bytes -> bits
     const algorithm = { name: 'AES-GCM', iv, tagLength, additionalData };
-
-    // chatty && log(`${this.mode} iv`, iv.join(' '));
-    // chatty && log(`${this.mode} input`, { algorithm, key: await cs.exportKey('jwk', this.key), data });
-
-    process.stdout.write('i' + this.mode.charAt(0) + record + '.');
-    const resultPromise = cs[this.mode](algorithm, this.key, data);
-
-    const { prevPromise } = this;
-    this.prevPromise = resultPromise;
-
-    const [resultBuffer] = await Promise.all([resultPromise, prevPromise]);  // ensure the Promises we return always resolve in call order
+    const resultBuffer = await cs[this.mode](algorithm, this.key, data);
     const result = new Uint8Array(resultBuffer);
-    process.stdout.write('o' + this.mode.charAt(0) + record + '.');
-    // chatty && log(`${this.mode} output`, result);
-
     return result;
   }
 }
